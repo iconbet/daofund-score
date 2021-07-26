b6d602f5872cd0840707eb3c6c50f2a155849fa6
cupiddev <cupiddev@protonmail.com>
2021-07-27 01:14:31 +0545

    Withdraw Record - VarDb for withdraw count - DictDb for tracking the withdraw record - Get Withdraw Record with amount, timestamp and address - Get withdraw count

--- daofund/daofund.py
+++ daofund/daofund.py
@@ -21,6 +21,8 @@ class DaoFund(IconScoreBase):
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
         self.admins = ArrayDB('admins', db, value_type=Address)
+        self.withdraw_count = VarDB('withdraw_count', db, value_type=int)
+        self.withdraw_record = DictDB('withdraw_record', db, value_type=str, depth=2)
 
     def on_install(self) -> None:
         super().on_install()
@@ -28,6 +30,7 @@ class DaoFund(IconScoreBase):
     def on_update(self) -> None:
         super().on_update()
         self.admins.put(self.owner)
+        self.withdraw_count.set(0)
 
     @external(readonly=True)
     def name(self) -> str:
@@ -81,6 +84,12 @@ class DaoFund(IconScoreBase):
         _available_amount = self.icx.get_balance(self.address)
         if _available_amount >= _amount:
             try:
+                self.withdraw_count.set(self.withdraw_count.get() + 1)
+                _withdraw_count: int = self.withdraw_count.get()
+                self.withdraw_record[_withdraw_count]['withdraw_amount'] = str(_amount)
+                self.withdraw_record[_withdraw_count]['withdraw_address'] = str(_address)
+                self.withdraw_record[_withdraw_count]['withdraw_memo'] = _memo
+                self.withdraw_record[_withdraw_count]['withdraw_timestamp'] = str(self.now() // 10 ** 6)
                 self.icx.transfer(_address, _amount)
                 self.FundTransferred(_address, f"{_amount} transferred to {_address} for {_memo}")
             except BaseException as e:
@@ -88,6 +97,19 @@ class DaoFund(IconScoreBase):
 
         else:
             revert(f"{TAG} :Not Enough balance. Available Balance = {_available_amount}.")
+
+    @external(readonly=True)
+    def get_withdraw_count(self) -> int:
+        return self.withdraw_count.get()
+
+    @external(readonly=True)
+    def get_withdraw_records(self) -> list:
+        wd_count: int = self.withdraw_count.get()
+        return [{"withdraw_address": self.withdraw_record[_withdraw]['withdraw_address'],
+                 "withdraw_timestamp": self.withdraw_record[_withdraw]['withdraw_timestamp'],
+                 "withdraw_reason": self.withdraw_record[_withdraw]['withdraw_memo'],
+                 "withdraw_amount": self.withdraw_record[_withdraw]['withdraw_amount']}
+                for _withdraw in range(1, wd_count + 1)]
 
     @payable
     def fallback(self):
